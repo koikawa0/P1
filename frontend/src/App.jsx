bd4a5e36e7fc4d4356e1f0126effe202126a74f0
koikawa <kaiofittipaldisc@gmail.com>
2025-12-01 18:29:26 -0300

    deploying

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+export default function Page() {
+  return <main>
+    <img src="./foto.svg" alt="foto" width="128"/>
+    <div>
+      <b>Koikawa</b> | 00 Anos | Web Developer
+    </div>
+    <div>
+      <b>Profile</b> | Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quia facere labore ex sunt minus eligendi et. Nesciunt tempora in cum nostrum eaque autem cupiditate praesentium necessitatibus rerum distinctio, voluptatibus hic.
+    </div>
+    <div>
+      <b>Experiences</b> | Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam dolores labore voluptas. Rem reiciendis accusamus suscipit magnam explicabo veniam tenetur porro ad nemo. Doloribus iusto vitae odit, quam ipsum provident!
+    </div>
+    <div>
+      <b>Competences</b> :
+      Logics,
+      Math,
+      JavaScript,
+      C#,
+      HTML,
+      Javascript and
+      React.
+    </div>
+    <div>
+      <a href="abyss@email.com">Email</a>
+      <a href="phone:+55(17)992168726">Phone</a>
+      <a href="https://github.com/koikawa0">Github</a>
+    </div>
+  </main>
 }
-
-export default App
